7bf6dc034b96df4646953a2898d06e756d7a7883
DotOfNotGot <37877493+DotOfNotGot@users.noreply.github.com>
2022-05-06 16:20:41 +0200

    HOLE EVEN BETTER WOWW

--- Assets/Scripts/BallLayerHandler.cs
+++ Assets/Scripts/BallLayerHandler.cs
@@ -10,23 +10,19 @@ public class BallLayerHandler : MonoBehaviour
     [SerializeField]
     private int layerOnTriggerExit;
 
-    // Changes layer of the ball object so that it stop colliding with the ground
-    // allowing it to fall down into the hole
+    // Changes layer of the other object so that it stops colliding with the ground
+    // allowing it to fall down into the hole. Also changes the layerOnTriggerExit
+    // integer so that it sets the object back to the correct layer after it exits.
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
-        {
-            other.gameObject.layer = layerOnTriggerEnter;
-        }
+        layerOnTriggerExit = other.gameObject.layer;
+        other.gameObject.layer = layerOnTriggerEnter;
     }
 
     // Changes layer back to the Ball layer so that it can collide with the ground again.
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Player")
-        {
             other.gameObject.layer = layerOnTriggerExit;
-        }   
     }
 
 
